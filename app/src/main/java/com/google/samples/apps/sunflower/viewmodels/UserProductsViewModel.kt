1e0539bf730a51fa37730c075dc751cd623fc445
engprodigy <kolbello@gmail.com>
2019-02-04 09:02:58 +0100

    buy button update on insurance product view

--- app/src/main/java/com/google/samples/apps/sunflower/viewmodels/UserProductsViewModel.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/viewmodels/UserProductsViewModel.kt
@@ -23,7 +23,7 @@ class UserProductViewModel : ViewModel() {
 
     }
 
-    fun getUsers() {
+    /*fun getUsers() {
         var retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -50,7 +50,7 @@ class UserProductViewModel : ViewModel() {
             }
         })
     }
-
+*/
 
 }
 
